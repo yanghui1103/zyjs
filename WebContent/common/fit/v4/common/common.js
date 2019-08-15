65ee75a789a2134d6154d985f48c25b8c9caee8d
yanghui1103 <yanghui1103@foxmail.com>
2019-08-15 20:48:09 +0800

    更换地址

--- WebContent/common/fit/v4/common/common.js
+++ WebContent/common/fit/v4/common/common.js
@@ -4,9 +4,9 @@
 *	2，通用的JS方法
 *	3，其他
 ****/
-var ctx = "http://localhost:8080/zyjs/";
+//var ctx = "http://localhost:8080/zyjs/";
 //var ctx = "http://10.1.9.160:8099/zyjs/";
-//var ctx = "http://zhjy.bwtech.top:8080/zyjs/";
+var ctx = "http://39.104.206.28:8080/zyjs/";
 var prompt_title = "系统提示框";
 
 /****
